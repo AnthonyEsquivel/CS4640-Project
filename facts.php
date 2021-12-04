70d3862607ea785313fd0cd507567f87ec23976b
AnthonyEsquivel <45054464+AnthonyEsquivel@users.noreply.github.com>
2021-12-03 20:08:14 -0500

    added html

--- facts.php
+++ facts.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
 header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT");
